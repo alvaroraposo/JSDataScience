29ac1d13450a511979e3d43a6b8a89ef11b2a2ec
Alvaro Raposo <alvaroraposo@yahoo.com.br>
2024-03-27 14:32:04 -0400

    mean absolute value

--- app/api/model/melbourneModel.ts
+++ app/api/model/melbourneModel.ts
@@ -2,8 +2,7 @@ import dfd from "danfojs-node";
 import tfjs from "@tensorflow/tfjs-node";
 import sk from "scikitjs";
 
-(async () => {
-  sk.setBackend(tfjs);
+const getXY = async () => {
   let melbourne_data = await dfd.readCSV("../data/melb_data.csv");
   melbourne_data = melbourne_data.dropNa({ axis: 1 });
 
@@ -17,10 +16,40 @@ import sk from "scikitjs";
   ];
 
   const x = melbourne_data.loc({ columns: melbourne_features });
+
+  return [x, y];
+};
+const firstMLModel = async () => {
+  const [x, y] = await getXY();
+
   const melbourne_model = new sk.DecisionTreeRegressor();
   melbourne_model.fit(x, y);
 
   const xHeadArray = await x.head().tensor.array();
-  x.head().print();
+  //x.head().print();
   console.log(melbourne_model.predict(xHeadArray));
+};
+
+const modelValidation = async () => {
+  const [x, y] = await getXY();
+  const xArray = await x.tensor.array();
+  const yArray = await y.tensor.array();
+  const [trainX, valX, trainY, valY] = sk.trainTestSplit(
+    xArray,
+    yArray,
+    undefined,
+    undefined,
+    1
+  );
+  const melbourne_model = new sk.DecisionTreeRegressor();
+  melbourne_model.fit(trainX, trainY);
+
+  const predictedHomePrices = melbourne_model.predict(valX);
+
+  console.log(sk.metrics.meanAbsoluteError(valY, predictedHomePrices));
+};
+
+(async () => {
+  sk.setBackend(tfjs);
+  await modelValidation();
 })();
--- app/api/model/trainModel.ts
+++ app/api/model/trainModel.ts
@@ -14,8 +14,7 @@ const getHousesPerYear = async (path: string | null | undefined = null) => {
   return dfd.toJSON(subDf);
 };
 
-(async () => {
-  sk.setBackend(tfjs);
+const getXY = async () => {
   let train_data = await dfd.readCSV("../data/train.csv");
   train_data = train_data.dropNa({ axis: 1 });
 
@@ -30,12 +29,42 @@ const getHousesPerYear = async (path: string | null | undefined = null) => {
   ];
 
   const x = train_data.loc({ columns: train_features });
+
+  return [x, y];
+};
+
+const firstMLModel = async () => {
+  const [x, y] = await getXY();
   const train_model = new sk.DecisionTreeRegressor();
   train_model.fit(x, y);
 
   const xHeadArray = await x.head().tensor.array();
   x.head().print();
   console.log(train_model.predict(xHeadArray));
+};
+
+const modelValidation = async () => {
+  const [x, y] = await getXY();
+  const xArray = await x.tensor.array();
+  const yArray = await y.tensor.array();
+  const [trainX, valX, trainY, valY] = sk.trainTestSplit(
+    xArray,
+    yArray,
+    undefined,
+    undefined,
+    1
+  );
+  const trainModel = new sk.DecisionTreeRegressor();
+  trainModel.fit(trainX, trainY);
+
+  const predictedHomePrices = trainModel.predict(valX);
+
+  console.log(sk.metrics.meanAbsoluteError(valY, predictedHomePrices));
+};
+
+(async () => {
+  sk.setBackend(tfjs);
+  await modelValidation();
 })();
 
 export { getHousesPerYear };
